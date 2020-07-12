d35bb4639db13d414457a9884878713b5a3c3027
Marwin Solomon <67198269+MarwinSol05@users.noreply.github.com>
2020-07-12 19:59:33 +0530

    Plot 3

---
+++ figure/Plot3.R
@@ -0,0 +1,35 @@
+setwd("C:\\Users\\MARK\\Desktop\\Marwin Documents\\Coursera Courses\\4. Exploratory Data Analysis\\Week 1\\Course Project 1")
+
+library(dplyr)
+library(lubridate)
+
+data<-read.table("./household_power_consumption.txt",sep=";")
+head(data,n=6)
+
+#Subsetting Data & Cleaning Data
+head(data$V1)
+head(dmy(data$V1))
+which(dmy(data$V1)=="2007-02-01")
+which(dmy(data$V1)=="2007-02-02")
+
+dat<-data[66638:69517,]
+head(dat)
+tail(dat)
+names(dat)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+rownames(dat)<-c(1:2880)
+head(dat)
+tail(dat)
+
+#Plotting
+sub1<-as.numeric(as.character(dat$Sub_metering_1))
+sub2<-as.numeric(as.character(dat$Sub_metering_2))
+sub3<-as.numeric(as.character(dat$Sub_metering_3))
+plot(c(1:2880),sub1,type="l",ylab="Energy sub metering",xlab="Days",xaxt="n",col="black",ylim=c(0,40))
+lines(c(1:2880),sub2,type="l",xaxt="n",col="red")
+lines(c(1:2880),sub3,type="l",xaxt="n",col="blue")
+axis(side=1, at=0, labels ="Thu")
+axis(side=1,at=1440,labels="Fri")
+axis(side=1, at=2880, labels ="Sat")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=1)
+dev.copy(png,file="Plot3.png",height=480,width=480)
+dev.off()
